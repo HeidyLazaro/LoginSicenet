1f60671e42b85736c143c0e4ec532940327172fc
EduardoLJ <edwardelric1291@gmail.com>
2024-02-16 00:23:58 -0600

    Estructura base terminada.

--- app/src/main/java/net/heidylazaro/loginsicenet/gui/screens/LoginViewModel.kt
+++ app/src/main/java/net/heidylazaro/loginsicenet/gui/screens/LoginViewModel.kt
@@ -1,2 +1,19 @@
 package net.heidylazaro.loginsicenet.gui.screens
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel(){
+    var loginUiState: String by mutableStateOf("")
+        private set
+
+    init {
+        getLoginSicenet()
+    }
+
+    fun getLoginSicenet() {
+        loginUiState = "Set the Sicenet status response here!"
+    }
+}
